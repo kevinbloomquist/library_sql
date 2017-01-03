be41c3060212312e2599971800067b28b05498ff
kevinbloomquist <rkevinbloom@gmail.com>
2017-01-03 16:50:14 -0700

    All searches

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -2,17 +2,22 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+	-- SELECT * FROM books JOIN authors ON (authors.id=books.author_id AND authors.id=8);
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+	-- SELECT * FROM books JOIN authors ON (authors.id=books.author_id AND authors.name='Milan Kundera');
 -- Find all books written by an author from China or the UK.
+	--SELECT * FROM books JOIN authors ON (authors.id=books.author_id) WHERE nationality='China' OR nationality='United Kingdom';
 
 -- Find out how many books Albert Camus wrote.
+	--SELECT * FROM books JOIN authors ON (authors.id=books.author_id AND name='Albert Camus');
 
 -- Find out how many books written before 1980 were by authors not from the US.
+	--SELECT * FROM books JOIN authors ON (authors.id=books.author_id AND publication_date<1980 AND nationality!='United States of America');
 
 -- For these last two, you should not need a JOIN.
 
 -- Find all authors whose names start with 'J'.
+	--SELECT * FROM authors WHERE name LIKE 'J%';
 
 -- Find all books whose titles contain 'the'.
+	--SELECT * FROM books WHERE title LIKE '%the%';
--- basic_queries.sql
+++ basic_queries.sql
@@ -2,30 +2,42 @@
 -- Unless otherwise stated, all queries should return all columns
 
 -- Get all information about all authors
+	-- SELECT * FROM authors
 
 -- Get just the name and birth year of all authors
+	-- SELECT name,birth_year FROM authors;
 
 -- Get all authors born in the 20th centruy or later
+	-- SELECT * FROM authors WHERE birth_year>1900;
 
 -- Get all authors born in the USA
+	-- SELECT * FROM authors WHERE nationality='United States of America';
 
 -- Get all books published on 1985
+	-- SELECT * FROM books WHERE publication_date='1985'; 
 
 -- Get all books published before 1989
+	-- SELECT * FROM books WHERE publication_date<'1989' ;
 
 -- Get just the title of all books.
+	-- SELECT title FROM books ;
 
 -- Get just the year that 'A Dance with Dragons' was published
+	-- SELECT publication_date FROM books WHERE title='A Dance with Dragons';
   -- Cry when you realize how long it's been
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+	-- SELECT * FROM books WHERE title Like '%the%';
 -- Add yourself as an author
+	-- INSERT INTO authors VALUES (9,'Kevin Bloomquist','United States of America',1978);
 
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
+	--INSERT INTO books (id,title,publication_date,author_id) VALUES (69,'How I Learned To SQL',2017,9),('70','I Got A JOB',2017,9);
 -- Update one of your books to have a new title
+	-- UPDATE books SET title='I GOT A SIX- FIGURE JOB!' WHERE title='I Got A JOB';
 
 -- Delete your books
+	--DELETE FROM books WHERE author_id=9;
 
 -- Delete your author entry
+	--DELETE FROM authors WHERE name= 'Kevin Bloomquist';
